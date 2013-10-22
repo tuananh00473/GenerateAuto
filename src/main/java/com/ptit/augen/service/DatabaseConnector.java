f6d1e8d7363bf213e265648cb0c9f2b1fb601412
anhnt <tuananh00473@gmail.com>
2013-10-22 18:02:46 +0700

    commit file

--- src/main/java/com/ptit/augen/service/DatabaseConnector.java
+++ src/main/java/com/ptit/augen/service/DatabaseConnector.java
@@ -6,7 +6,6 @@ import com.ptit.augen.model.Table;
 import com.ptit.augen.ultility.Constants;
 import com.ptit.augen.ultility.GlobalVariables;
 import com.ptit.augen.ultility.StringExecuteConverter;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
@@ -89,20 +88,18 @@ public class DatabaseConnector
     @ResponseBody
     JsonResult loadDatabase() throws JSONException, SQLException
     {
-        JSONObject loadDatabaseResponse = new JSONObject();
+        JsonResult jsonResult = new JsonResult();
         if (GlobalVariables.Status == Constants.ConnectionEmpty)
         {
-            loadDatabaseResponse.put("status", "empty");
-            loadDatabaseResponse.put("data", new ArrayList());
-//            return loadDatabaseResponse;
-            return null;
+            jsonResult.setStatus("empty");
+            jsonResult.setData(new ArrayList());
+            return jsonResult;
         }
         if (GlobalVariables.Status == Constants.ConnectionError)
         {
-            loadDatabaseResponse.put("status", "error");
-            loadDatabaseResponse.put("data", new ArrayList());
-//            return loadDatabaseResponse;
-            return null;
+            jsonResult.setStatus("error");
+            jsonResult.setData(new ArrayList());
+            return jsonResult;
         }
         Connection connection = GlobalVariables.connection;
 
@@ -122,12 +119,8 @@ public class DatabaseConnector
             }
         }
 
-        loadDatabaseResponse.put("status", "success");
-        loadDatabaseResponse.put("data", tables);
-        JsonResult jsonResult = new JsonResult();
         jsonResult.setStatus("success");
         jsonResult.setData(tables);
-//        return loadDatabaseResponse;
         return jsonResult;
     }
 
@@ -231,12 +224,12 @@ public class DatabaseConnector
         return false;
     }
 
-    @RequestMapping(value = "/Connection/GetListFieldByTable", params = {"TableName"}, method = RequestMethod.POST)
+    @RequestMapping(value = "/Connection/GetListFieldByTable", params = {"TableName"}, method = RequestMethod.GET)
     public
     @ResponseBody
-    String getListFieldByTable(@RequestParam String TableName) throws SQLException, JSONException
+    JsonResult getListFieldByTable(@RequestParam String TableName) throws SQLException, JSONException
     {
-        JSONArray fieldArray = new JSONArray();
+        JsonResult jsonResult = new JsonResult();
         Connection connection = GlobalVariables.connection;
         DatabaseMetaData dataBaseMetaData = connection.getMetaData();
         String fieldPrimaryKeyName = null;
@@ -247,24 +240,62 @@ public class DatabaseConnector
         }
 
         ResultSet resultSetColumn = dataBaseMetaData.getColumns(null, null, TableName, "%");
-        List<Field> fields = new ArrayList<Field>();
+        ArrayList<Field> fields = new ArrayList<Field>();
         while (resultSetColumn.next())
         {
-            JSONObject fieldJSON = new JSONObject();
             Field field = new Field();
+
             String columnName = resultSetColumn.getString(4);
             String columnType = resultSetColumn.getString(6);
 
-            field.setFieldName(columnName);
-            field.setFieldType(columnType);
+            field.setFieldName(columnName);  // 1. none 2. .. 3. .. 4. column name 5. .. 6. type data
+            field.setFieldType(StringExecuteConverter.convertTypeOfDataToExtentionJSType(columnType));
+            field.setServerFieldType(StringExecuteConverter.convertTypeOfDataToJavaType(columnType));
             field.setIsKey(checkPrimaryKey(columnName, fieldPrimaryKeyName));
 
-            fieldJSON.put("columnName", field.getFieldName());
-            fieldJSON.put("columnType", field.getFieldType());
-            fieldJSON.put("isKey", field.getIsKey());
+            field.setIsInt(false);
+            field.setIsLong(false);
+            field.setIsBoolean(false);
+            field.setIsFloat(false);
+            field.setIsString(false);
 
-            fieldArray.put(fieldJSON);
+            if (field.getFieldType().equals("int"))
+            {
+                field.setIsInt(true);
+            }
+            else if (field.getFieldType().equals("auto"))
+            {
+                field.setIsLong(true);
+            }
+            else if (field.getFieldType().equals("boolean"))
+            {
+                field.setIsBoolean(true);
+            }
+            else if (field.getFieldType().equals("double"))
+            {
+                field.setIsFloat(true);
+            }
+            else if (field.getFieldType().equals("string"))
+            {
+                field.setIsString(true);
+            }
+
+            if (!field.getFieldName().isEmpty() && !field.getIsKey())
+            {
+                field.setLabelName(field.getFieldName());
+                field.setFieldWidth("500");
+                field.setFieldHeight("28");
+                field.setItemType(Constants.DATA_FIELD_TYPE.get(0));
+                field.setAllowBlank("false");
+                field.setAllowBlankBoolean(false);
+                field.setIsHTMLEditor(false);
+                field.setHasSearch(false);
+                field.setSearchType("");
+            }
+            fields.add(field);
         }
-        return fieldArray.toString();
+        jsonResult.setStatus("success");
+        jsonResult.setData(fields);
+        return jsonResult;
     }
 }
